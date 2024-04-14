28784302fba82d416381239ab99b69f4d848d1bd
JLpro-cd <75441530+JLpro-cd@users.noreply.github.com>
2024-04-13 19:28:38 -0700

    v3

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -36,7 +36,7 @@ const users = {
   };
   
   const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
+    users["users_list"].find((user) => user["id"] === id);
 
   app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
@@ -46,6 +46,17 @@ const users = {
     } else {
       res.send(result);
     }
+  });
+
+  const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+  };
+
+  app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
   });
 
   app.listen(port, () => {
